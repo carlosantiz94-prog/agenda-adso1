1c0293fb68e5872a580e1571a61bf370753d366d
Carlos Santis <carlosantiz94@gmail.com>
2025-10-28 19:27:12 -0500

    nuevos componentes

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,12 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
