5dbe00606511af80a03769985a8ae34232c8ad7c
Carlos Santis <carlosantiz94@gmail.com>
2025-10-28 19:48:49 -0500

    actualizacion agenda

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 // Este componente arma la pantalla completa y usa ContactoCard para cada persona.
 import "./App.css"; // Importamos estilos de la app
-import ContactoCard from "./components/ContactoCard"; // Importamos el componente hijo
+import ContactoCard from "./components/ContactoCard.jsx"; // Importamos el componente hijo
 
 export default function App() {
   // Esta es nuestra "base de datos" inicial quemada en el código
@@ -26,6 +26,48 @@ export default function App() {
       telefono: "320 555 7788",
       correo: "luisa@sena.edu.co",
       etiqueta: "Cliente",
+    },
+    {
+      id: 4,
+      nombre: "Pedro Sánchez",
+      telefono: "321 765 4321",
+      correo: "pedro@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+    {
+      id: 5,
+      nombre: "María Sánchez",
+      telefono: "321 987 6543",
+      correo: "maria@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 6,
+      nombre: "Ana Pérez",
+      telefono: "322 765 4321",
+      correo: "ana@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+    {
+      id: 7,
+      nombre: "Juan Pérez",
+      telefono: "323 987 6543",
+      correo: "juan@sena.edu.co",
+      etiqueta: "Instructor",
+    },
+    {
+      id: 8,
+      nombre: "Luis Pérez",
+      telefono: "324 765 4321",
+      correo: "luis@sena.edu.co",
+      etiqueta: "Cliente",
+    },
+    {
+      id: 9,
+      nombre: "María Pérez",
+      telefono: "325 987 6543",
+      correo: "maria@sena.edu.co",
+      etiqueta: "Instructor",
     },
   ];
 
