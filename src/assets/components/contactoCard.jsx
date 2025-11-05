63a11b8a5eee09263cef97977a4cde3faf7b73ca
Carlos Santiz <carlosantiz94@gmail.com>
2025-11-04 20:25:21 -0500

    clase 5 v3

--- src/assets/components/contactoCard.jsx
+++ src/assets/components/contactoCard.jsx
@@ -11,7 +11,7 @@ export default function ContactoCard({
       <h3>{nombre}</h3>
       <p>📱 {telefono}</p>
       <p>✉️ {correo}</p>
-      <p>{empresa}</p>
+      {empresa && <p>μ {empresa}</p>}
       {etiqueta && <span className="tag">{etiqueta}</span>}
       <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
         Eliminar
